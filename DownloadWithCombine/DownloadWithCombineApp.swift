e504dbd7ba38b4dd3ba5e59e5b46b004a43bdbde
Brian McIntosh <brian@mixinteractive.com>
2023-04-13 13:46:55 -0400

    Initial Commit

---
+++ DownloadWithCombine/DownloadWithCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadWithCombineApp.swift
+//  DownloadWithCombine
+//
+//  Created by Brian McIntosh on 4/13/23.
+//
+
+import SwiftUI
+
+@main
+struct DownloadWithCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
